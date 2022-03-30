81e6fe87b1cea9751828fb3dc3adfb1cbf2b4956
James Lovejoy <jameslovejoy1@gmail.com>
2022-03-30 15:29:34 -0400

    Add library from aleth for ZK precompiles

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -15,3 +15,11 @@ hunter_config(
     URL https://github.com/chfast/intx/archive/v0.10.1.tar.gz
     SHA1 168e0c2ff6760bd6467e25523cc0e68447358018
 )
+
+hunter_config(
+    libff
+    VERSION 0.2.1
+    URL https://github.com/scipr-lab/libff/archive/refs/tags/v0.2.1.tar.gz
+    SHA1 7736613185125ceb542927aa81b426207efb7ec6
+    CMAKE_ARGS OPENSSL_ROOT_DIR=/usr/local/opt/openssl WITH_PROCPS=OFF
+)
